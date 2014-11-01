be5e095a3b311decad6b2512f552df1178e4e28c
Michael Blix <mike@quasi.co>
2014-11-01 17:34:02 -0400

    misc cleanup

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 'use strict';
 
-var camera, scene, renderer;
+var clock = new THREE.Clock();
 
-var time = Date.now(), clock = new THREE.Clock();
+var camera, scene, renderer;
 
 var fullScreenButton;
 
 var geometry, material, mesh;
-
-var effect;
-var controls;
 
 var vrEffect;
 var vrControls;
@@ -33,35 +30,19 @@ function load() {
 function init() {
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 3000);
 
-	scene = new THREE.Scene();
-	scene.fog = new THREE.Fog(0xffffff, 0, 1500);
-
-	fullScreenButton = document.querySelector('.button');
-
-	controls = new THREE.OculusRiftControls(camera);
-	scene.add(controls.getObject());
+	fullScreenButton = document.querySelector('#vr-button');
 
 	setupScene();
 	setupLights();
 	setupRendering();
-
 	setupEvents();
 }
 
-function setupLights() {
-	var light = new THREE.DirectionalLight(0xffffff, 1.5);
-	light.position.set(1, 1, 1);
-	scene.add(light);
-
-	light = new THREE.DirectionalLight(0xffffff, 0.75);
-	light.position.set(-1, -0.5, -1);
-	scene.add(light);
-
-	light = new THREE.AmbientLight(0x666666);
-	scene.add(light);
-}
 
 function setupScene() {
+	scene = new THREE.Scene();
+	scene.fog = new THREE.Fog(0xffffff, 0, 1500);
+
 	// floor
 	var geometry = new THREE.PlaneGeometry(2000, 2000, 1, 1);
 	geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
@@ -83,15 +64,15 @@ function setupScene() {
 	// cubes
 	geometry = new THREE.BoxGeometry(30, 30, 30);
 
-	for (var i = 0; i < 200; i ++) {
+	for (var i = 0; i < 500; i ++) {
 
 		var object = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: Math.random() * 0xffffff } ) );
 
 		object.material.ambient = object.material.color;
 
-		object.position.x = Math.random() * 1000 - 500;
-		object.position.y = Math.random() * 600 - 300;
-		object.position.z = Math.random() * 800 - 400;
+		object.position.x = Math.random() * 2000 - 1000;
+		object.position.y = Math.random() * 1000 - 500;
+		object.position.z = Math.random() * 2000 - 1000;
 
 		object.rotation.x = Math.random() * 2 * Math.PI;
 		object.rotation.y = Math.random() * 2 * Math.PI;
@@ -110,16 +91,24 @@ function setupScene() {
 	}
 }
 
+function setupLights() {
+	var light = new THREE.DirectionalLight(0xffffff, 1.5);
+	light.position.set(1, 1, 1);
+	scene.add(light);
+
+	light = new THREE.DirectionalLight(0xffffff, 0.75);
+	light.position.set(-1, -0.5, -1);
+	scene.add(light);
+
+	light = new THREE.AmbientLight(0x666666);
+	scene.add(light);
+}
+
 function setupRendering() {
 	renderer = new THREE.WebGLRenderer({
-		antialias: false,
+		antialias: false
 	});
-	renderer.setClearColor(0xffffff, 1);
-
-	var DK1 = [1280, 800];
-	var DK2 = [1920, 1080];
-
-	var HMD = DK1;
+	renderer.setClearColor(0xffffff, 0);
 
 	function VREffectLoaded(error) {
 		if (error) {
@@ -132,9 +121,7 @@ function setupRendering() {
 	vrEffect = new THREE.VREffect(renderer, VREffectLoaded);
 	vrControls = new THREE.VRControls(camera);
 
-	var container = document.createElement('div');
-	document.body.insertBefore(container, document.body.firstChild);
-	container.appendChild(renderer.domElement);
+	document.body.appendChild(renderer.domElement, document.body.firstChild);
 }
 
 function setupEvents() {
@@ -154,32 +141,43 @@ function onWindowResize() {
 }
 
 function keyPressed (e) {
-	if (e.keyCode == 'R'.charCodeAt(0)) {
-		console.log(vrControls.getVRState());
-		vrControls._vrInput.resetSensor();
+	switch (e.keyCode) {
+		case 'R'.charCodeAt(0):
+			vrControls._vrInput.zeroSensor();
+			break;
+		case 'F'.charCodeAt(0):
+			vrEffect.setFullScreen(true);
+			break;
 	}
 }
 
 function animate(t) {
 	requestAnimationFrame(animate);
 
+	var dt = clock.getDelta();
+
 	var vrState = vrControls.getVRState();
 
 	var s = 500;
 
-	var cPos = controls.getObject().position;
-	var vrPos = vrState.hmd.position;
-	var pos = vrPos;
-	pos[0] *= s;
-	pos[1] *= s;
-	pos[2] *= s;
+	if (vrState) {
+		var vrPos = vrState.hmd.position;
+		var pos = vrPos;
+		pos[0] *= s;
+		pos[1] *= s;
+		pos[2] *= s;
 
-	camera.position.fromArray(pos);
-	controls.update(Date.now() - time, vrState);
+		camera.position.fromArray(pos);
+	}
 
 	vrControls.update();
-	vrEffect.render(scene, camera);
 
-	time = Date.now();
+	render(dt);
 }
+
+function render(dt) {
+	vrEffect.render(scene, camera);
+}
+
+
 
